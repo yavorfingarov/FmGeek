f4085d6d49ad172703c85d1361dee4187505d6ae
Yavor Fingarov <yavor.fingarov@outlook.com>
2024-03-06 18:32:13 +0200

    Fixed dark theme (#17)

--- src/manifest.js
+++ src/manifest.js
@@ -20,7 +20,8 @@ export const manifest = {
         {
             src: "pwa-512x512.png",
             sizes: "512x512",
-            type: "image/png"
+            type: "image/png",
+            purpose: "any"
         },
         {
             src: "maskable-icon-512x512.png",
