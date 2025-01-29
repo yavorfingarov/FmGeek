e9283c632ac2155312af19d2405de1bcefa98f07
Yavor Fingarov <yavor.fingarov@outlook.com>
2025-01-29 23:09:54 +0200

    Fixed station order

--- src/components/settings.default.js
+++ src/components/settings.default.js
@@ -326,6 +326,11 @@ export const defaultStations = [
                 website: "https://www.wbgo.org/"
             },
             {
+                name: "WBGO The Jazz Bee",
+                stream: "https://wbgo.streamguys1.com/thejazzstream",
+                website: "https://www.wbgo.org/"
+            },
+            {
                 name: "Jazz 88 San Diego",
                 stream: "https://ksds-ice.streamguys1.com/ksds.mp3",
                 website: "https://www.jazz88.org/"
@@ -339,11 +344,6 @@ export const defaultStations = [
                 name: "KMHD Portland",
                 stream: "https://ais-sa3.cdnstream1.com/2442_128.aac",
                 website: "https://www.kmhd.org/"
-            },
-            {
-                name: "WBGO The Jazz Bee",
-                stream: "https://wbgo.streamguys1.com/thejazzstream",
-                website: "https://www.wbgo.org/"
             },
             {
                 name: "WWFM New Jersey JazzOn2",
