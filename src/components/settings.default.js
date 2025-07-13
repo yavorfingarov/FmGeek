3b2e808fb1e2a350853239feeaf65d84fbc06efd
Yavor Fingarov <29012836+yavorfingarov@users.noreply.github.com>
2025-07-14 01:06:02 +0300

    Added preload hint (#91)

--- src/components/settings.default.js
+++ src/components/settings.default.js
@@ -296,11 +296,6 @@ export const defaultStations = [
                 website: "https://www1.wdr.de/radio/wdr3/index.html"
             },
             {
-                name: "PR Niezły Jazz",
-                stream: "https://stream85.polskieradio.pl/omniaaxe/k103.stream/playlist.m3u8",
-                website: "https://moje.polskieradio.pl/#/115?gsid=106"
-            },
-            {
                 name: "CRo Jazz",
                 stream: "https://rozhlas.stream/jazz_high.aac",
                 website: "https://jazz.rozhlas.cz/"
